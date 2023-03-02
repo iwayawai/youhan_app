28165c7403378948b1b6b1751f1305f28c055dcf
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-02 13:18:22 +0000

    Update recipe#new

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -5,5 +5,15 @@ class Public::RecipesController < ApplicationController
     @genres=Genre.all
   end
   
+  def create
+    @recipe=Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to recipe_path(@recipe.id)
+  end 
+  
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :introduction, :star, :genre_id, :time, :price, :ingredient_id, :procedure, :point, :video, :image)
+  end  
 
 end
