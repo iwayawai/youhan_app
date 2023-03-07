9dee24ba6a878cae0c089536217093bef87cdf06
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-07 10:47:50 +0000

    Update users#unsubscribe

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,4 +6,21 @@ class Public::UsersController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end 
   
+  def edit
+    @user = current_user
+  end
+  
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice]= "退会処理を実行いたしました"
+    redirect_to root_path
+  end 
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password, :is_deleted, :image)
+  end
+  
 end
