919b8cd4b33032213a15ae1152017ad186d84c23
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-13 11:58:18 +0000

    Update search

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -24,24 +24,34 @@ class Public::RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end 
   
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe.id)
+  end
+  
   def index
     if params[:search] == nil
-    @recipes = Recipe.page(params[:page])
+      @recipes = Recipe.page(params[:page])
     elsif params[:search] == ''
-    @recipes = Recipe.page(params[:page])
+      @recipes = Recipe.page(params[:page])
     else
-    @recipes = Recipe.where("ingredient LIKE ? or name LIKE ? ",'%' + params[:search] + '%','%' + params[:search] + '%').page(params[:page])
+      genre = Genre.find_by(name: params[:search]) 
+      @recipes = Recipe.where("ingredient LIKE ? or name LIKE ? or genre_id LIKE ?",'%' + params[:search] + '%','%' + params[:search] + '%', genre&.id).page(params[:page])
     end 
     
-    if params[:star] == 1
-      @recipes = Recipe.where(star:'1').page(params[:page])
-    elsif params[:star] == 2
-      @recipes = Recipe.where(star:'2').page(params[:page])
-    elsif params[:star] == 3
-      @recipes = Recipe.where(star:'3').page(params[:page])
-    else
-      @recipes = Recipe.page(params[:page])
-    end 
+    if params[:star].present?
+      if params[:star] == "1"
+        @recipes = Recipe.where(star:'1').page(params[:page])
+      elsif params[:star] == "2"
+        @recipes = Recipe.where(star:'2').page(params[:page])
+      elsif params[:star] == "3"
+        @recipes = Recipe.where(star:'3').page(params[:page])
+      else
+        @recipes = Recipe.page(params[:page])
+      end
+    end
+    
   end 
   
   private
