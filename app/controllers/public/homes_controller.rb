f73e184a02bef7408291091161bc24e4358487b2
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-13 12:57:10 +0000

    Update Homes#top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,7 @@ class Public::HomesController < ApplicationController
   
   def top
     @user = current_user
+    @recipes = Recipe.find(Favorite.group(:recipe_id).order('count(recipe_id) desc').limit(3).pluck(:recipe_id))
   end
   
   def about
