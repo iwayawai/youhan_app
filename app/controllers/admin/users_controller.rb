0834ef6b140b0a4179f547d73fe55d7a2f944902
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-14 13:28:58 +0000

    Create Adimin#userspage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,18 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.page(params[:page])
+  end 
+  
+  
+  private
+  def user_params
+    params.require(:user).permit(
+      :name, 
+      :email, 
+      :encrypted_password, 
+      :is_deleted, 
+      :image)
+  end
+  
 end
