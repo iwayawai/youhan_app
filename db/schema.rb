ada713b7ca433d6e525ac6ec6b9844009aa361f6
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-14 14:12:19 +0000

    Update Users#index

--- db/schema.rb
+++ db/schema.rb
@@ -91,11 +91,11 @@ ActiveRecord::Schema.define(version: 2023_02_27_074250) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.boolean "is_deleted", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.boolean "is_deleted"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
