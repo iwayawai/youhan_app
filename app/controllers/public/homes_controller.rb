a998d56bdec9eeaa5bb4d462b505a82014e041a4
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-03-16 13:07:48 +0000

    Update top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,12 +1,17 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
     @user = current_user
-    @recipes = Recipe.find(Favorite.group(:recipe_id).order('count(recipe_id) desc').limit(3).pluck(:recipe_id))
+    @recipes = []
+    3.times do |star|
+      @recipes << Recipe.find(Favorite.joins(:recipe).where("recipes.star = ?", star + 1).group(:recipe_id).order('count(recipe_id) desc').limit(1).pluck(:recipe_id))
+    end
+    # convert nested array to even 
+    @recipes.flatten!
   end
-  
+
   def about
     @user = current_user
-  end 
+  end
 
 end
