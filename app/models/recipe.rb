b4b7eb9402d4b2b56c258c918e97af6a45ba4aa3
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-04-04 12:57:33 +0000

    Fix Recipe#show

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,13 +6,13 @@ class Recipe < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :star, presence: { message: "を選んでください" }
   validates :genre, presence: { message: "を選んでください" }
-  validates :image, presence: { message: "か動画を投稿してください" }
   validates :introduction, presence: true, length: { maximum: 100 }
   validates :time, presence: true
   validates :price, presence: true
   validates :ingredient, presence: true
   validates :procedure, presence: true
   validates :point, presence: true, length: { maximum: 50 }
+  validates :required_either_image_or_video, presence: true
 
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -23,5 +23,12 @@ class Recipe < ApplicationRecord
    def favorited_by?(user)
      favorites.exists?(user_id: user.id)
    end
+   
+   def required_either_image_or_video
+     return if image.present? ^ video.present?
 
+     errors.add(:base, '画像または動画のどちらか一方を投稿してください')
+   end
+       
 end
+
