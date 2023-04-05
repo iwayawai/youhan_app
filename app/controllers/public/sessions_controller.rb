edab803621267d7f75299f02c52f87c21d4fd35d
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-04-05 12:12:56 +0000

    Fix Sessions#new

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+   before_action :user_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
   # GET /resource/sign_in
   # def new
@@ -24,18 +25,18 @@ class Public::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to user_path(user.id), notice: 'ゲストユーザーとしてログインしました。'
   end
-  
+
   protected
   def user_state
-    @user = User.find_by(email: params[:user][:email])
+    @user = User.find_by(email:params[:user][:email])
     return if !@user
-    if @user.valid_password?(params[:user][:email]) && (@user.is_deleted == true)
+    if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
        redirect_to new_user_registration_path
     else
-       flash[:notice] = "項目を入力してください。" 
+       flash[:notice] = "項目を入力してください。"
     end
-  end 
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
